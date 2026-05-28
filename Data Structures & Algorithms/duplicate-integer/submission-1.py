d3f4a75461f951b6c04a298d8f6dda7425902e09
Phillip <phillip.banky@gmail.com>
2026-05-28 11:30:07 -0700

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        listOfNums = []
+
+        for i in nums:
+            if i in listOfNums:
+                return True
+            listOfNums.append(i)
+        
+        return False
---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        duplicates = {}
+
+        for i in range(len(nums)):
+            if nums[i] in duplicates:
+                return True
+            duplicates[nums[i]] = 1
+        
+        return False
